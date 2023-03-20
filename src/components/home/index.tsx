692c2c2a784451d1d1c1bd971d816e516bc0ba7d
RiqueA2001 <henriquealmada2001@gmail.com>
2023-03-20 20:39:22 -0300

    Change home text and head icon

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -7,7 +7,9 @@ const Home = () => {
       <div className="absolute top-0 left-0 bg-black opacity-[0.5] w-full h-full"></div>
       <div className="text-center text-white z-20">
         <h1 className="text-[2.2rem] sm:text-[4rem]">Henrique Almada</h1>
-        <h2 className="sm:text-[1.4rem] text-[#9799a1]">DESENVOLVEDOR WEB</h2>
+        <h2 className="sm:text-[1.4rem] text-[#9799a1]">
+          DESENVOLVEDOR FRONT-END
+        </h2>
       </div>
     </section>
   )
